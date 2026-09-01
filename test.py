4abdaf27edc21860482ec857a6d58df1a703df62
rohith564 <mrohith0089@gmail.com>
2026-09-01 21:06:53 +0530

    api key changed

--- test.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,render_template,request
-
-app = Flask (__name__)
-
-@app.route("/",methods=["POST","GET"])
-
-def testing():
-    return render_template("calculator.html")
-
